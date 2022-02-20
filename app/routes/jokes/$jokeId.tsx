e75845d3a281b07ee7060bd06829f24d5d2af2e9
pravtz <ederson.ufrgs@gmail.com>
2022-02-20 10:02:08 -0300

    add delet jokes

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,21 @@
-import { LoaderFunction, useParams, Link, useLoaderData, useCatch} from "remix";
+import type { ActionFunction, LoaderFunction } from "remix";
+import {
+  Link,
+  useLoaderData,
+  useCatch,
+  redirect,
+  useParams
+} from "remix";
 import type { Joke } from "@prisma/client";
 
 import { db } from "~/utils/db.server";
 
-type LoaderData = { joke: Joke };
+import { requireUserId,getUserId } from "~/utils/session.server";
 
-export const loader: LoaderFunction = async ({params}) => {
+type LoaderData = { joke: Joke; isOwner: boolean };
+
+export const loader: LoaderFunction = async ({request, params }) => {
+  const userId = await getUserId(request);
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId }
   });
@@ -14,8 +24,40 @@ export const loader: LoaderFunction = async ({params}) => {
       status: 404
     });
   }
-  const data: LoaderData = { joke };
+  const data: LoaderData = { joke, isOwner: userId === joke.jokesterId};
   return data;
+};
+
+export const action: ActionFunction = async ({
+  request,
+  params
+}) => {
+  const form = await request.formData();
+  if (form.get("_method") !== "delete") {
+    throw new Response(
+      `The _method ${form.get("_method")} is not supported`,
+      { status: 400 }
+    );
+  }
+  const userId = await requireUserId(request);
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId }
+  });
+  if (!joke) {
+    throw new Response("Can't delete what does not exist", {
+      status: 404
+    });
+  }
+  if (joke.jokesterId !== userId) {
+    throw new Response(
+      "Pssh, nice try. That's not your joke",
+      {
+        status: 401
+      }
+    );
+  }
+  await db.joke.delete({ where: { id: params.jokeId } });
+  return redirect("/jokes");
 };
 
 export default function JokeRoute() {
@@ -26,20 +68,50 @@ export default function JokeRoute() {
       <p>Here's your hilarious joke:</p>
       <p>{data.joke.content}</p>
       <Link to=".">{data.joke.name} Permalink</Link>
+      {data.isOwner ? (
+        <form method="post">
+          <input
+            type="hidden"
+            name="_method"
+            value="delete"
+          />
+          <button type="submit" className="button">
+            Delete
+          </button>
+        </form>
+      ) : null}
     </div>
   );
 }
 export function CatchBoundary() {
   const caught = useCatch();
   const params = useParams();
-  if (caught.status === 404) {
-    return (
-      <div className="error-container">
-        Huh? What the heck is "{params.jokeId}"?
-      </div>
-    );
+  switch (caught.status) {
+    case 400: {
+      return (
+        <div className="error-container">
+          What you're trying to do is not allowed.
+        </div>
+      );
+    }
+    case 404: {
+      return (
+        <div className="error-container">
+          Huh? What the heck is {params.jokeId}?
+        </div>
+      );
+    }
+    case 401: {
+      return (
+        <div className="error-container">
+          Sorry, but {params.jokeId} is not your joke.
+        </div>
+      );
+    }
+    default: {
+      throw new Error(`Unhandled error: ${caught.status}`);
+    }
   }
-  throw new Error(`Unhandled error: ${caught.status}`);
 }
 export function ErrorBoundary() {
   const { jokeId } = useParams();
